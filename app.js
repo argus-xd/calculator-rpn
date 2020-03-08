6a80a8bbd0838ba3aff8ad271c7f32fbd02c155d
argus-xd <xxxARGUSxxx58@gmail.com>
2020-03-08 20:56:19 +0500

    delete import postFix

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 const readline = require("readline");
 const calc = require("./calc");
-const postFix = require("./postFix");
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
     terminal: false
 });
-/* 
-console.log(calc.postFixCalc("-12 12 +")); */
 
 rl.on("line", function(line) {
     if (line) {
